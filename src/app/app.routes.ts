6e8febd530d0614afba2102cdb8d52ed709754b2
Cristian Bedoya Vargas <cristianbedoyavargas@gmail.com>
2024-10-14 19:57:29 -0500

    lazy loading routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,15 @@ import { TaskFormComponent } from './pages/task-form/task-form.component';
 export const routes: Routes = [
     {
         path: 'task-list',
-        component: TaskListComponent
+        component: TaskListComponent,
+        loadComponent: () => 
+            import('./pages/task-list/task-list.component').then((m) => m.TaskListComponent)
     },
     {
         path: 'create-task',
-        component: TaskFormComponent
+        component: TaskFormComponent,
+        loadComponent: () => 
+            import('./pages/task-form/task-form.component').then((m) => m.TaskFormComponent)
     },
     { path: '**', redirectTo: 'task-list'}
 ];
